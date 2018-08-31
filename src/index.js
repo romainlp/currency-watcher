a4602b829dc464694bdab4bc83ae47d6ec494196
Romain Le Polh <romainlepolh@gmail.com>
2018-08-31 16:46:59 +0930

    Base store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import rootReducer from './store'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(rootReducer)
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+)
+registerServiceWorker()
