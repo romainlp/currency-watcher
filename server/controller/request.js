833487764364296d0cff779df6e74e7d8b695c72
Romain Le Polh <romainlepolh@gmail.com>
2018-09-05 10:13:17 +0930

    Add gitignore

--- server/controller/request.js
+++ server/controller/request.js
@@ -1,40 +1,55 @@
 const axios = require('axios');
+const moment = require('moment');
 const config = require('../config.js');
 const Request = require('../model/Request');
 
 /**
  * Fetch datas from Transferwise API
- * - We save the request every hours
+ * - We save the request every minutes
  */
 exports.fetch = async (ctx) => {
-  let args = {
-    'amount': config.CURRENCY_AMOUNT,
-    'amountCurrency': 'source',
-    'hasDiscount': false,
-    'isFixedRate': false,
-    'isGuaranteedFixedTarget': false,
-    'sourceCurrency': config.CURRENCY_FROM,
-    'targetCurrency': config.CURRENCY_TO
-  }
-  try {
-    const response = await axios.get(
-      config.TRANSFERWISE_URL, 
-      {
-        params: args,
-        crossdomain: true,
-        headers: {
-          'X-Authorization-key': config.TRANSFERWISE_KEY
-        }
-      })
-    const request = Request.create({
-      currencyFrom: config.CURRENCY_FROM,
-      currencyTo: config.CURRENCY_TO,
-      datas: response.data
+  const lastRequest = await Request.findOne(
+    {}, 
+    {}, 
+    {
+      sort: { 'date' : -1 }
     })
-    ctx.body = response.data
-  } catch (error) {
-    console.log(error)
-    ctx.body = error
+  if (lastRequest) {
+    const dateRequest = moment(lastRequest.date)
+    if (moment().isSame(dateRequest, 'minute')) {
+      ctx.body = lastRequest
+    } else {
+      console.log('Fetch new request ', moment().format())
+      let args = {
+        'amount': config.CURRENCY_AMOUNT,
+        'amountCurrency': 'source',
+        'hasDiscount': false,
+        'isFixedRate': false,
+        'isGuaranteedFixedTarget': false,
+        'sourceCurrency': config.CURRENCY_FROM,
+        'targetCurrency': config.CURRENCY_TO
+      }
+      try {
+        const response = await axios.get(
+          config.TRANSFERWISE_URL, 
+          {
+            params: args,
+            crossdomain: true,
+            headers: {
+              'X-Authorization-key': config.TRANSFERWISE_KEY
+            }
+          })
+        const request = Request.create({
+          currencyFrom: config.CURRENCY_FROM,
+          currencyTo: config.CURRENCY_TO,
+          datas: response.data
+        })
+        ctx.body = response.data
+      } catch (error) {
+        console.log(error)
+        ctx.body = error
+      }
+    }
   }
 }
 
