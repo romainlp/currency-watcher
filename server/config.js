106cd5ce2116f2a00aa9eb78ba2f17b20c4c4e35
Romain Le Polh <romainlepolh@gmail.com>
2018-09-06 09:22:47 +0930

    Update interval

--- server/config.js
+++ server/config.js
@@ -36,4 +36,4 @@ exports.CURRENCIES = [
 
 // Misc
 exports.DEFAULT_NUMBER_RESULTS = process.env.DEFAULT_NUMBER_RESULTS ? process.env.DEFAULT_NUMBER_RESULTS : 15
-exports.FETCH_INTERVAL = process.env.FETCH_INTERVAL ? process.env.FETCH_INTERVAL : 10000 // 5 minutes
+exports.FETCH_INTERVAL = process.env.FETCH_INTERVAL ? process.env.FETCH_INTERVAL : 60000 // 1 minutes
