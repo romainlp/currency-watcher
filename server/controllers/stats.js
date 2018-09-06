ece0fe62bd4d2221cee1c5665216eda0327da5e5
Romain Le Polh <romainlepolh@gmail.com>
2018-09-06 13:22:44 +0930

    Add Statts

---
+++ server/controllers/stats.js
@@ -0,0 +1,88 @@
+const axios = require('axios');
+const moment = require('moment');
+const config = require('../config.js');
+const Rate = require('../models/Rate');
+
+const getRates = async (ctx, startDate) => {
+  return await Rate.find({
+      currencyFrom: ctx.params.from,
+      currencyTo: ctx.params.to,
+      date: {
+        $gte: startDate
+      }
+    },
+    null,
+    {
+      sort: { 'date' : -1 }
+    }
+  );
+}
+
+const day = async (ctx) => {
+  const startDate = moment().hours(0).minutes(0).seconds(0).milliseconds(0).format("YYYY-MM-DD")
+  const rates = await getRates(ctx, startDate)
+
+  if (rates) {
+    const firstRate = rates[rates.length - 1]
+    const lastRate = rates[0]
+
+    ctx.body = {
+      firstRate: firstRate,
+      lastRate: lastRate,
+      diff: lastRate.rate - firstRate.rate
+    }
+  }
+}
+
+const week = async (ctx) => {
+  const startDate = moment().startOf('week').format("YYYY-MM-DD")
+  const rates = await getRates(ctx, startDate)
+
+  if (rates) {
+    const firstRate = rates[rates.length - 1]
+    const lastRate = rates[0]
+
+    ctx.body = {
+      firstRate: firstRate,
+      lastRate: lastRate,
+      diff: lastRate.rate - firstRate.rate
+    }
+  }
+}
+
+const month = async (ctx) => {
+  const startDate = moment().startOf('month').format("YYYY-MM-DD")
+  const rates = await getRates(ctx, startDate)
+  
+  if (rates) {
+    const firstRate = rates[rates.length - 1]
+    const lastRate = rates[0]
+
+    ctx.body = {
+      firstRate: firstRate,
+      lastRate: lastRate,
+      diff: lastRate.rate - firstRate.rate
+    }
+  }
+}
+
+const year = async (ctx) => {
+  const startDate = moment().startOf('year').format("YYYY-MM-DD")
+  const rates = await getRates(ctx, startDate)
+  
+  if (rates) {
+    const firstRate = rates[rates.length - 1]
+    const lastRate = rates[0]
+
+    ctx.body = {
+      firstRate: firstRate,
+      lastRate: lastRate,
+      diff: lastRate.rate - firstRate.rate
+    }
+  }
+}
+
+module.exports.day = day
+module.exports.week = week
+module.exports.month = month
+module.exports.year = year
